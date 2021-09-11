76ad1bbdf022944d46157091433b6e37db3a9358
hacper <git@hacperme.com>
2021-09-12 02:55:27 +0800

    add python-3.7.6rc1-embed-amd64

--- env.bat
+++ env.bat
@@ -5,6 +5,7 @@ set cur_dir=%~dp0
 set CMAKE_PATH=%cur_dir%tools\cmake\cmake-3.21.2-windows-i386\bin
 set MINGW_PATH=%cur_dir%tools\mingw730_32
 set TOOLCHAIN_PATH=%cur_dir%tools\mingw730_32\bin
-set PATH=%CMAKE_PATH%;%MINGW_PATH%;%TOOLCHAIN_PATH%
+set PYTHON_PATH=%cur_dir%tools\python-3.7.6rc1-embed-amd64
+set PATH=%CMAKE_PATH%;%MINGW_PATH%;%TOOLCHAIN_PATH%;%PYTHON_PATH%
 echo %PATH%
 
