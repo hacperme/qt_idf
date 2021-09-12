ffb2ca4ed93012a28b8ae7b7c36b74c542c42870
hacper <git@hacperme.com>
2021-09-12 11:03:12 +0800

    install kconfiglib

--- env.bat
+++ env.bat
@@ -6,6 +6,8 @@ set CMAKE_PATH=%cur_dir%tools\cmake\cmake-3.21.2-windows-i386\bin
 set MINGW_PATH=%cur_dir%tools\mingw730_32
 set TOOLCHAIN_PATH=%cur_dir%tools\mingw730_32\bin
 set PYTHON_PATH=%cur_dir%tools\python-3.7.6rc1-embed-amd64
-set PATH=%CMAKE_PATH%;%MINGW_PATH%;%TOOLCHAIN_PATH%;%PYTHON_PATH%
+set PYTHON_SCRIPTS_PATH=%PYTHON_PATH%\Scripts
+set PYTHON_LIB_PATH=%PYTHON_PATH%\Lib\site-packages
+set PATH=%CMAKE_PATH%;%MINGW_PATH%;%TOOLCHAIN_PATH%;%PYTHON_PATH%;%PYTHON_SCRIPTS_PATH%;%PYTHON_LIB_PATH%
 echo %PATH%
 
