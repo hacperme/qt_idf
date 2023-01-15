b40fc2ff442a110a028d0b036d713d13a6cd2b59
hacper <git@hacperme.com>
2023-01-15 14:15:42 +0800

    feat: add SDL2

--- examples/shell/main.c
+++ examples/shell/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include "nr_micro_shell.h"
+#include <SDL2/SDL.h>
 
-int main(void)
+static SDL_Thread *nr_shell_thread = NULL;
+
+static int nr_shell_thread_entry(void *data)
 {
     char c;
     shell_init();
@@ -10,4 +13,15 @@ int main(void)
         c = getchar();
         shell(c);
     }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+
+    SDL_Init(SDL_INIT_VIDEO);
+    nr_shell_thread = SDL_CreateThreadWithStackSize(nr_shell_thread_entry, "nr_shell_thread_entry", 2 * 1024, NULL);
+    SDL_WaitThread(nr_shell_thread, NULL);
+    SDL_Quit();
+    return 0;
 }
